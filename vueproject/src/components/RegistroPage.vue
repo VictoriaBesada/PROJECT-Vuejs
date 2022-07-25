d0d4f831fb9f393982760e27a410f4f2cd6ea76c
Victoria <vikibesada99@gmail.com>
2022-07-25 19:24:11 -0300

    vue forms

--- vueproject/src/components/RegistroPage.vue
+++ vueproject/src/components/RegistroPage.vue
@@ -12,66 +12,157 @@
                       Registrate!
                     </p>
 
-                    <form class="mx-1 mx-md-4">
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example1c"
-                            class="form-control"
-                            v-model="usuario.nombre"
-                          />
-                          <label class="form-label" for="form3Example1c"
-                            >Nombre</label
-                          >
+                    <vue-form
+                      :state="formstate"
+                      @submit.prevent="onSubmit"
+                      class="mx-1 mx-md-4"
+                    >
+                      <validate auto-label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <span class="form-label" for="form3Example1c"
+                              >Nombre</span
+                            >
+                            <input
+                              required
+                              name="name"
+                              class="form-control"
+                              v-model.lazy="usuario.name"
+                            />
+                            <field-messages
+                              name="name"
+                              show="$touched || $submitted"
+                              class="form-control-feedback"
+                            >
+                              <div slot="required">
+                                Nombre es un campo requerido
+                              </div>
+                            </field-messages>
+                          </div>
                         </div>
-                      </div>
+                      </validate>
 
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example3c"
-                            class="form-control"
-                            v-model="usuario.apellido"
-                          />
-                          <label class="form-label" for="form3Example3c"
-                            >Apellido</label
-                          >
-                        </div>
-                      </div>
+                      <br />
 
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example4c"
-                            class="form-control"
-                            v-model="usuario.usuario"
-                          />
-                          <label class="form-label" for="form3Example4c"
-                            >Usuario</label
-                          >
+                      <validate auto-label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <div class="input-group mb-3">
+                              <span
+                                class="input-group-text"
+                                for="inputGroupSelect01"
+                                style="text-align: center"
+                                >Pais</span
+                              >
+                              <select
+                                required
+                                v-model.lazy="usuario.pais"
+                                class="form-select"
+                                name="pais"
+                                id="inputGroupSelect01"
+                              >
+                                <option selected>Elegir...</option>
+                                <option value="Argentina">Argentina</option>
+                                <option value="Brasil">Brasil</option>
+                                <option value="Chile">Chile</option>
+                                <option value="Paraguay">Paraguay</option>
+                                <option value="Uruguay">Uruguay</option>
+                              </select>
+                              <field-messages
+                                name="pais"
+                                show="$touched || $submitted"
+                                class="form-control-feedback"
+                              >
+                                <div slot="required">
+                                  Pais es un campo requerido
+                                </div>
+                              </field-messages>
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      </validate>
 
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4cd"
-                            class="form-control"
-                            v-model="usuario.password"
-                          />
-                          <label class="form-label" for="form3Example4cd"
-                            >Contrasena</label
-                          >
+                      <validate auto-label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <span class="form-label" for="form3Example4c"
+                              >Edad</span
+                            >
+                            <input
+                              required
+                              name="edad"
+                              type="number"
+                              class="form-control"
+                              v-model.number="usuario.edad"
+                            />
+                            <field-messages
+                              name="edad"
+                              show="$touched || $submitted"
+                              class="form-control-feedback"
+                            >
+                              <div slot="required">
+                                Edad es un campo requerido
+                              </div>
+                              <div slot="edad">
+                                La edad minima es de 21 anios
+                              </div>
+                            </field-messages>
+                          </div>
                         </div>
-                      </div>
+                      </validate>
+
+                      <validate auto-label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <span class="form-label" for="form3Example4c"
+                              >Email</span
+                            >
+                            <input
+                              required
+                              name="email"
+                              type="email"
+                              class="form-control"
+                              v-model.lazy="usuario.email"
+                            />
+                            <field-messages
+                              name="email"
+                              show="$touched || $submitted"
+                              class="form-control-feedback"
+                            >
+                              <div slot="required">
+                                Email es un campo requerido
+                              </div>
+                              <div slot="email">Email no es válido</div>
+                            </field-messages>
+                          </div>
+                        </div>
+                      </validate>
+
+                      <validate auto-label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <span class="form-label" for="form3Example4cd"
+                              >Contrasenia</span
+                            >
+                            <input
+                              required
+                              name="password"
+                              type="password"
+                              class="form-control required-field"
+                              v-model.lazy="usuario.password"
+                            />
+                            <field-messages
+                              name="password"
+                              show="$touched || $submitted"
+                              class="form-control-feedback"
+                            >
+                              <div slot="required">
+                                Password es un campo requerido
+                              </div>
+                            </field-messages>
+                          </div>
+                        </div>
+                      </validate>
 
                       <div
                         class="form-check d-flex justify-content-center mb-5"
@@ -91,11 +182,13 @@
                       <div
                         class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
                       >
-                        <button type="button" class="btn btn-primary btn-lg">
-                          Registrarme
-                        </button>
+                        <input
+                          type="submit"
+                          value="Registrarme"
+                          class="btn btn-primary btn-lg"
+                        />
                       </div>
-                    </form>
+                    </vue-form>
                   </div>
                   <div
                     class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2"
@@ -113,6 +206,74 @@
         </div>
       </div>
     </section>
+    <section style="background-color: #eee">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" style="border-radius: 25px">
+              <div class="card-body p-md-5">
+                <div class="row justify-content-center">
+                  <table class="table align-middle mb-0 bg-white">
+                    <thead class="bg-light">
+                      <tr>
+                        <th>Nombre</th>
+                        <th>Pais</th>
+                        <th>Edad</th>
+                        <th>Email</th>
+                        <th>Contrasenia</th>
+                        <th>Eliminar datos</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <div class="d-flex align-items-center">
+                            <div class="ms-3">
+                              <p class="text-success fw-bold mb-1">
+                                {{ usuario.name }}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <p class="text-success fw-bold mb-1">
+                            {{ usuario.pais }}
+                          </p>
+                        </td>
+                        <td>
+                          <p class="text-success fw-bold mb-1">
+                            {{ usuario.edad }}
+                          </p>
+                        </td>
+                        <td>
+                          <p class="text-success fw-bold mb-1">
+                            {{ usuario.email }}
+                          </p>
+                        </td>
+                        <td>
+                          <p class="text-success fw-bold mb-1">
+                            {{ usuario.password }}
+                          </p>
+                        </td>
+                        <td>
+                          <button
+                            type="button"
+                            class="btn btn-link btn-sm btn-rounded"
+                            v-on:click="limpiar"
+                          >
+                            Limpiar
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -122,17 +283,35 @@ export default {
   props: [],
   data() {
     return {
+      formstate: {
+        $error: {},
+        $submittedState: {},
+      },
       usuario: {
-        nombre: "",
-        apellido: "",
-        usuario: "",
+        name: "",
+        pais: "",
+        edad: "",
+        email: "",
         password: "",
       },
     };
   },
   methods: {
     registrar() {
-      this.$emit('enviarUsuarios', this.usuario)
+      this.$emit("enviarUsuarios", this.usuario);
+    },
+    onSubmit: function () {
+      if (this.formstate.$invalid) {
+        alert(this.formstate.$error);
+        return;
+      }
+    },
+    limpiar (){
+        (this.usuario.name = '',
+        this.usuario.pais = '',
+        this.usuario.edad = '',
+        this.usuario.email = '',
+        this.usuario.password = '')
     },
   },
 };
@@ -141,5 +320,8 @@ export default {
 <style scoped>
 .form-check-input {
   position: relative;
+}
+.input-group {
+  flex-direction: column;
 }
 </style>
--- vueproject/src/main.js
+++ vueproject/src/main.js
@@ -1,21 +1,29 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import VueForm from "vue-form";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueForm, {
+  inputClasses: {
+    valid: "form-control-success",
+    invalid: "form-control-danger",
+  },
+});
 
 Vue.mixin({
-  methods:{},
-  data(){
-    return{
-      usuario: '',
-      password: ''
-    }
-  }
-})
+  methods: {},
+  data() {
+    return {
+      usuario: "",
+      password: "",
+    };
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
