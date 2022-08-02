b154ad55bb69511f364fecbe1532e52516867bfb
Victoria <vikibesada99@gmail.com>
2022-08-02 20:58:19 -0300

    login validators

--- vueproject/src/components/LoginPage.vue
+++ vueproject/src/components/LoginPage.vue
@@ -6,41 +6,80 @@
           <div class="col">
             <div class="card">
               <div class="card-body p-4">
-                <div class="mb-3 row">
-                  <label for="name" class="col-sm-2 col-form-label"
-                    >Usuario</label
-                  >
-                  <div class="col-sm-10">
-                    <input
-                      type="text"
-                      name="name"
-                      class="form-control"
-                      id="usuario"
-                      v-model="nombre"
-                    />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="password" class="col-sm-2 col-form-label"
-                    >Contrasena</label
-                  >
-                  <div class="col-sm-10">
-                    <input
-                      type="password"
-                      name="password"
-                      class="form-control"
-                      id="inputPassword"
-                      v-model="password"
-                    />
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  class="btn btn-primary btn-lg"
-                  @click="cambiarValor"
+                <vue-form
+                  :state="formstate"
+                  @submit.prevent="onSubmit"
+                  autocomplete="off"
+                  class="mx-1 mx-md-4"
                 >
-                  Entrar
-                </button>
+                  <validate auto-label>
+                    <div class="mb-3 row">
+                      <label for="nombre" class="col-sm-2 col-form-label"
+                        >Usuario</label
+                      >
+                      <div class="col-sm-10">
+                        <input
+                          required
+                          name="nombre"
+                          minlength="5"
+                          maxlength="12"
+                          class="form-control"
+                          type="text"
+                          v-model.lazy="nombre"
+                        />
+                        <field-messages
+                          name="nombre"
+                          show="$touched || $submitted"
+                          class="form-control-feedback"
+                        >
+                          <div slot="required">
+                            Usuario es un campo requerido
+                          </div>
+                          <div slot="minlength">
+                            Usuario debe tener entre 5 y 12 caracteres
+                          </div>
+                        </field-messages>
+                      </div>
+                    </div>
+                  </validate>
+                  <validate auto-label>
+                    <div class="mb-3 row">
+                      <label for="password" class="col-sm-2 col-form-label"
+                        >Contrasena</label
+                      >
+                      <div class="col-sm-10">
+                        <input
+                          required
+                          minlength="5"
+                          maxlength="12"
+                          name="password"
+                          type="password"
+                          class="form-control required-field"
+                          v-model.lazy="password"
+                        />
+                        <field-messages
+                          name="password"
+                          show="$touched || $submitted"
+                          class="form-control-feedback"
+                        >
+                          <div slot="required">
+                            Password es un campo requerido
+                          </div>
+                          <div slot="minlength">
+                            Password debe tener entre 5 y 12 caracteres
+                          </div>
+                        </field-messages>
+                      </div>
+                    </div>
+                  </validate>
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-lg"
+                    @click="cambiarValor"
+                  >
+                    Entrar
+                  </button>
+                </vue-form>
               </div>
             </div>
           </div>
@@ -58,6 +97,10 @@ export default {
   },
   data() {
     return {
+      formstate: {
+        // $error: {},
+        $submittedState: {},
+      },
       nombre: "",
       password: "",
     };
@@ -69,6 +112,13 @@ export default {
     //     this.$emit("changeFlag");
     //   }
     // },
+    onSubmit: function () {
+      if (this.formstate.$invalid) {
+        alert("Hay errores en el ingreso");
+        return;
+      }
+      console.log("Usuario logueado!", this.formstate);
+    },
     cambiarValor() {
       if (this.nombre == "admin" && this.password == "123") {
         this.$emit("changeFlag");
